08c676809f4a8d4c5dbf69145e698e17a22e7c56
Victor Mas Montserrat <victormasmon@gmail.com>
2023-06-04 19:25:31 +0200

    Saving testing code into quote spider

---
+++ scraping_time/fotocasa/fotocasa/spiders/quotes.py
@@ -0,0 +1,34 @@
+import scrapy
+import time
+from ..items import FotocasaItem
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = ["https://quotes.toscrape.com/"]
+
+    def parse(self, response):
+
+        items = FotocasaItem()
+
+        # Goes throw every tag with the class quote
+        for quotes in response.css('div.quote'):
+
+            # Extracts the iterated quotes
+            items["text"] = quotes.css('.text::text').extract()[0]\
+                .replace('“', '')\
+                .replace('”', '')\
+                .replace("'", "")
+            items["author"] = quotes.css('.author::text').extract()[0]
+            items["url_author"] = quotes.css(
+                'div.quote > span > a::attr(href)').extract()[0]
+            items["tags"] = quotes.css('.tag::text').extract()[0]
+
+            yield items
+        # yield {"text": text, "autor": autor, 'url_autor': url_autor}
+
+        # if exists goes to the next page
+        next_page = response.css(".next a::attr(href)").get()
+        if next_page is not None:
+            time.sleep(2)  # Sleeps a while to avoid being banned
+            yield response.follow(next_page, callback=self.parse)
