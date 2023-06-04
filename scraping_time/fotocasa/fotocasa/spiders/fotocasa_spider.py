2945e8f716ed9c7338f58ab5c55e8eeeb4b8d9d3
Victor Mas Montserrat <victormasmon@gmail.com>
2023-06-04 22:10:06 +0200

    Add use of splash for fotocasa

--- scraping_time/fotocasa/fotocasa/spiders/fotocasa_spider.py
+++ scraping_time/fotocasa/fotocasa/spiders/fotocasa_spider.py
@@ -1,16 +1,18 @@
 import scrapy
-from os import path
-from ..items import FotocasaItem
+from scrapy_splash import SplashRequest
+import time
+from ..items import FlatItem
+
 
 class FotoCasaSpider(scrapy.Spider):
     name = "fotocasa"
     start_urls = [
-        # "https://www.fotocasa.es/es/alquiler/viviendas/barcelona-capital/"
-        # "todas-las-zonas/l?conservationStatusIds=0&constructionTypeIds=1%"
-        # "2C2&maxPrice=1300&minRooms=3&minSurface=60&sortType=publication"
-        # "Date",
-        'https://quotes.toscrape.com/'
+        "https://www.fotocasa.es/es/alquiler/viviendas/barcelona-capital/todas-las-zonas/l?conservationStatusIds=0&constructionTypeIds=1%2C2&maxPrice=1300&minRooms=3&minSurface=60&sortType=publication"
     ]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield SplashRequest(url, self.parse, args={'wait': 0.5})
 
     def parse(self, response):
         # url = response.css("div.quote span.text::text").extract()
@@ -27,7 +29,7 @@ class FotoCasaSpider(scrapy.Spider):
         # TODO: Only charge the first image you need to work with javascript
         # img = response.css("article.re-CardPackMinimal > a > div > div > div > ul > li > img::attr(src)").extract()
 
-        items = FotocasaItem()
+        items = FlatItem()
 
         # Goes throw every tag with the class quote
         for quotes in response.css('div.quote'):
@@ -45,6 +47,11 @@ class FotoCasaSpider(scrapy.Spider):
             yield items
         # yield {"text": text, "autor": autor, 'url_autor': url_autor}
 
+        # if exists goes to the next page
+        next_page = response.css(".next a::attr(href)").get()
+        if next_page is not None:
+            time.sleep(2)  # Sleeps a while to avoid being banned
+            yield response.follow(next_page, callback=self.parse)
 
 # //*[@id="App"]/div[1]/div[3]/main/div/div[2]/section/article[1]/a
 # article.re-CardPackMinimal:nth-child(1) > a:nth-child(1)
